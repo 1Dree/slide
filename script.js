18b932178d297e9036a9223ed9a1742cd43145f2
1Dree <67266656+1Dree@users.noreply.github.com>
2020-12-10 08:24:05 -0300

    Add files via upload
    Projeto finalizado

--- script.js
+++ script.js
@@ -1,33 +1,60 @@
 ((doc) => {
   const $ = (el) => doc.querySelector(el),
-    slideItems = $("#slide #slide-items"),
-    slideItem = doc.querySelectorAll("#slide-items .slide-item"),
-    prevBtn = $("#slide #slide-btns button#previous"),
-    nextBtn = $("#slide #slide-btns button#next");
+    contentItems = $("#slide-content #content-items"),
+    slideItem = doc.querySelectorAll("#slide-content .slide-item"),
+    slideBtns = doc.querySelectorAll("#slide-btns i"),
+    slideMap = $("#slide #slide-map");
   let counter = 0;
 
-  nextBtn.addEventListener("click", function (e) {
-    counter++;
-    if (counter === slideItem.length) counter = 0;
-    nextBtn.disabled = true;
+  // renderizando o mapa do slide
+  const HTMLslideMap = (ref) => ref.map(() => "<span></span>").join("");
 
-    slideItems.style.transform = `translateX(${-100 * counter}%)`;
+  function renderSlideMap([...auxVector], target) {
+    const content = HTMLslideMap(auxVector);
 
-    slideItems.addEventListener("transitionend", () => {
-      nextBtn.disabled = false;
-    });
-  });
+    target.innerHTML = content;
+    target.children[0].classList.add("current");
+  }
 
-  prevBtn.addEventListener("click", function (e) {
-    if (counter === 0) counter = slideItem.length;
-    counter--;
+  renderSlideMap(slideItem, slideMap);
+  // fim
 
-    prevBtn.disabled = true;
+  const preventMultipleClicks = (elBtn, bool, fn) => {
+    bool === true
+      ? elBtn.removeEventListener("click", fn)
+      : elBtn.addEventListener("click", fn);
+  };
 
-    slideItems.style.transform = `translateX(${-100 * counter}%)`;
+  function counterConfigOfIncrement(direction, limit) {
+    if (direction === "next") {
+      counter++;
+      if (counter === limit) counter = 0;
+    } else {
+      if (counter === 0) counter = limit;
+      counter--;
+    }
+  }
 
-    slideItems.addEventListener("transitionend", () => {
-      prevBtn.disabled = false;
-    });
-  });
+  function changeLocations([...points], arrPosition) {
+    points.forEach((item) => item.classList.remove("current"));
+
+    points[arrPosition].classList.add("current");
+  }
+
+  function slide({ target }, receiver, _slideMap, slideLimit) {
+    preventMultipleClicks(target, true, execute);
+    receiver.addEventListener("transitionend", () =>
+      preventMultipleClicks(target, false, execute)
+    );
+
+    counterConfigOfIncrement(target.id, slideLimit);
+    changeLocations(_slideMap.children, counter);
+
+    receiver.style.transform = `translateX(${-100 * counter}%)`;
+  }
+
+  const execute = (event) =>
+    slide(event, contentItems, slideMap, slideItem.length);
+
+  slideBtns.forEach((btn) => btn.addEventListener("click", execute));
 })(document);
